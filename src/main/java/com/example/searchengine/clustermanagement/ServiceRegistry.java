fd7525a024f0ae96fc1e1100153a8d700c8d6867
XianzeCao <xcao@coveo.com>
2023-09-13 22:08:33 -0400

    added service registry class

--- src/main/java/com/example/searchengine/clustermanagement/ServiceRegistry.java
+++ src/main/java/com/example/searchengine/clustermanagement/ServiceRegistry.java
@@ -1,6 +1,36 @@
 package com.example.searchengine.clustermanagement;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.List;
+
 public class ServiceRegistry {
 
+    public static final String WORKERS_REGISTRY_ZNODE = "/workers_service_registry";
+    public static final String COORDINATORS_REGISTRY_ZNODE = "/coordinators_service_registry";
+    private final ZooKeeper zooKeeper;
+    private List<String> allServiceAddresses = null;
+    private String currentZnode = null;
+    private final String serviceRegistryZnode;
 
+    public ServiceRegistry(ZooKeeper zooKeeper, String serviceRegistryZnode) {
+        this.zooKeeper = zooKeeper;
+        this.serviceRegistryZnode = serviceRegistryZnode;
+        createServiceRegistryNode();
+    }
+
+    private void createServiceRegistryNode() {
+        try {
+            if (zooKeeper.exists(serviceRegistryZnode, false) == null) {
+                zooKeeper.create(serviceRegistryZnode, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
 }
